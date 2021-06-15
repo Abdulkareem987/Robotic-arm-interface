fe618bb8c176ac078ff056fde9a5fe1db7fb59f6
Abdulkareem987 <74791066+Abdulkareem987@users.noreply.github.com>
2021-06-16 01:55:28 +0300

    First

---
+++ Interface for Robotic Arm/JS/script.js
@@ -0,0 +1,35 @@
+var slider = document.getElementById("First");
+    var output = document.getElementById("Value1");
+     output.innerHTML = slider.value;
+              
+    slider.oninput = function() {
+    output.innerHTML = this.value;
+        }
+    var slider2 = document.getElementById("Second");
+    var output2 = document.getElementById("Value2");
+     output2.innerHTML = slider2.value;
+              
+    slider2.oninput = function() {
+    output2.innerHTML = this.value;
+        }
+    var slider3 = document.getElementById("Third");
+    var output3 = document.getElementById("Value3");
+     output3.innerHTML = slider3.value;
+              
+    slider3.oninput = function() {
+    output3.innerHTML = this.value;
+        }
+    var slider4 = document.getElementById("Fourth");
+    var output4 = document.getElementById("Value4");
+     output4.innerHTML = slider4.value;
+              
+    slider4.oninput = function() {
+    output4.innerHTML = this.value;
+        }
+    var slider5 = document.getElementById("Fifth");
+    var output5 = document.getElementById("Value5");
+     output5.innerHTML = slider5.value;
+              
+    slider5.oninput = function() {
+    output5.innerHTML = this.value;
+        }
