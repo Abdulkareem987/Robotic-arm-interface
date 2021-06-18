7b1d8c9c43c952f9edfcf1f7e0bf77a9eb1351ed
Abdulkareem987 <74791066+Abdulkareem987@users.noreply.github.com>
2021-06-18 18:49:05 +0300

    Run Table is done

--- Interface for Robotic Arm/Run.php
+++ Interface for Robotic Arm/Run.php
@@ -4,17 +4,55 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "roboticArmDB";
+$db = "DBRoboticArm";
 
-$conn = new mysqli('localhost','root','','roboticArmDB');
+$conn = new mysqli( $servername, $username, $password, $db);
 
 if ($conn->connect_error) {
 
     die("Connection failed: " . $conn->connect_error);
 }
 
-$_SESSION['Status'] = "Success!!";
-    header('location: Robotic Arm Interface.php');
+   $tbl_status = "CREATE TABLE IF NOT EXISTS Motor_Status (
+       id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+       motor_on BOOL 
+   )";
+
+            If($conn->query($tbl_status) === TRUE) 
+            {
+
+                
+
+                $insert_status = "INSERT INTO Motor_Status (motor_on) 
+                        VALUES (true)
+                        ";
+
+                if ($conn->query($insert_status) === FALSE) 
+                {
+                    echo "Error: " . $update_status . "<br>" . $conn->error;
+                    
+                } 
+
+                $last_id = $conn->insert_id;
+
+                $update_status = "UPDATE Motor_Status SET motor_on = 'false'
+                                  Where id != $last_id
+                        ";
+
+                if ($conn->query($update_status) === TRUE) 
+                {
+                    echo "Success!";
+                } 
+                else 
+                {
+                    echo "Error: " . $update_status . "<br>" . $conn->error;
+                }
+                            
+            }
+            else 
+            {
+                echo "Error: " . $tbl_status . "<br>" . $conn->error;
+            }
 
 $conn->close();
 ?>
--- Interface for Robotic Arm/Save.php
+++ Interface for Robotic Arm/Save.php
@@ -1,36 +1,67 @@
 <?php
 session_start();
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "roboticArmDB";
 
-$conn = new mysqli('localhost','root','','roboticArmDB');
+$conn = new mysqli('localhost','root','');
 
-if ($conn->connect_error) {
-
+if ($conn->connect_error) 
+ {
     die("Connection failed: " . $conn->connect_error);
-}
+ }
 
-$firstSlider = $_POST['firstSlider'];
-$secondSlider = $_POST['secondSlider'];
-$thirdSlider = $_POST['thirdSlider'];
-$fourthSlider = $_POST['fourthSlider'];
-$fifthSlider = $_POST['fifthSlider'];
-$sixthSlider = $_POST['sixthSlider'];
+    $sql = "CREATE DATABASE IF NOT EXISTS DBRoboticArm";
 
-$sql = "INSERT INTO motor_range (id ,firstMotor, secondMotor, thirdMotor, fourthMotor, fifthMotor, sixthMotor) 
-        VALUES ('','$firstSlider', '$secondSlider', '$thirdSlider', '$fourthSlider', '$fifthSlider', '$sixthSlider')";
+        If($conn->query($sql) === TRUE) 
+        {
 
-if ($conn->query($sql) === TRUE) {
-    
-    $_SESSION['Status'] = "Data has been saved!";
-    header('location: Robotic Arm Interface.php');
-} else {
+            $conn = new mysqli('localhost','root','', 'DBRoboticArm');
+                
+            $tbl_range = "CREATE TABLE IF NOT EXISTS Motor_Range (
+                    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    first_motor INT(11) NOT NULL,
+                    second_motor INT(11) NOT NULL,
+                    third_motor INT(11) NOT NULL,
+                    fourth_motor INT(11) NOT NULL,
+                    fifth_motor INT(11) NOT NULL,
+                    sixth_motor INT(11) NOT NULL
+                    )";
 
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+                If($conn->query($tbl_range) === TRUE) 
+                    {
+
+                        $firstSlider = $_POST['firstSlider'];
+                        $secondSlider = $_POST['secondSlider'];
+                        $thirdSlider = $_POST['thirdSlider'];
+                        $fourthSlider = $_POST['fourthSlider'];
+                        $fifthSlider = $_POST['fifthSlider'];
+                        $sixthSlider = $_POST['sixthSlider'];
+
+                        $insert_range = "INSERT INTO Motor_Range (id ,first_Motor, second_Motor, third_Motor, fourth_Motor, fifth_Motor, sixth_Motor) 
+                                VALUES ('','$firstSlider', '$secondSlider', '$thirdSlider', '$fourthSlider', '$fifthSlider', '$sixthSlider')";
+
+                        if ($conn->query($insert_range) === TRUE) 
+                        {
+                            $_SESSION['Status'] = "Data has been saved!";
+                            header('location: Robotic Arm Interface.php');
+                        } 
+                        else 
+                        {
+                            echo "Error: " . $insert_range . "<br>" . $conn->error;
+                        }
+                                    
+                    }
+                    else 
+                    {
+                        echo "Error: " . $tbl_range . "<br>" . $conn->error;
+                    }
+
+            $conn->close();
+
+        } 
+        else 
+        {
+            echo "Error creating database:" . $conn->error;
+        }
 
 $conn->close();
 ?>
